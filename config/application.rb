04c1407ce523d50ef20e7dc1cec429547cc03211
yuji yamada <ymdarudy@gmail.com>
2022-08-15 14:24:32 +0900

    :wrench: devise日本語化対応

--- config/application.rb
+++ config/application.rb
@@ -8,18 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Psikarte
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :ja
   end
 end
