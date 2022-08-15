6f5c196e0d0b7745795709a1250d14d7e43c605a
yuji yamada <ymdarudy@gmail.com>
2022-08-15 12:37:44 +0900

    :wrench: タイムゾーンを東京に設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Psikarte
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
