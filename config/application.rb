561604ee4dd17536b05941752f7bbe8b2fd26a52
yuji yamada <ymdarudy@gmail.com>
2022-08-16 14:55:32 +0900

    :wrench: アプリ名修正

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Psikarte
+module Psykarte
   class Application < Rails::Application
     config.load_defaults 6.1
 
