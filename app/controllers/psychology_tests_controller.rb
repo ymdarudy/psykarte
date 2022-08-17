3a616c521e472a1b13533b1e4eaaacff9ad84cbb
yuji yamada <ymdarudy@gmail.com>
2022-08-17 22:55:46 +0900

    :construction: 心理テスト回答ロジック（createアクション）実装

--- app/controllers/psychology_tests_controller.rb
+++ app/controllers/psychology_tests_controller.rb
@@ -5,5 +5,7 @@ class PsychologyTestsController < ApplicationController
 
   def show
     @psychology_test = PsychologyTest.find(params[:id])
+    session[:psychology_test_id] = @psychology_test.id
+    @answer = Answer.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
   resources :psychology_tests, only: %i[index show]
+  resources :answers, only: %i[create]
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
   devise_for :users
 end
