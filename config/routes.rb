e6c8ca66b3fd8b4069595cf77b7b152ae99998c2
yuji yamada <ymdarudy@gmail.com>
2022-08-19 17:06:50 +0900

    :construction: マイページ実装中（ルーティングと仮画面まで作成）

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :psychology_tests, only: %i[index show]
   resources :answers, only: %i[create show]
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
-  devise_for :users
+  devise_for :users, controllers: {
+            registrations: "users/registrations",
+          }
+  resources :users, only: %i[show]
 end
