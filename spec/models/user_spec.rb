d41c9132952d1049ca6451694ca40e940907d67d
yuji yamada <ymdarudy@gmail.com>
2022-08-24 18:05:31 +0900

    :rotating_light: Userモデルスペック記載。emailのバリデーション強化

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+    example "nameは必須、かつ50文字以下" do
+      user = User.new(name: "", email: "test@example.com", password: "password")
+      expect(user).to be_invalid
+
+      user = User.new(name: "a" * 51, email: "test@example.com", password: "password")
+      expect(user).to be_invalid
+
+      user = User.new(name: "山田", email: "test@example.com", password: "password")
+      expect(user).to be_valid
+    end
+
+    example "emailは必須、かつ所定のフォーマット" do
+      user = User.new(name: "山田", email: "", password: "password")
+      expect(user).to be_invalid
+
+      user = User.new(name: "山田", email: "test", password: "password")
+      expect(user).to be_invalid
+
+      user = User.new(name: "山田", email: "test@example", password: "password")
+      expect(user).to be_invalid
+
+      user = User.new(name: "山田", email: "test@example.com", password: "password")
+      expect(user).to be_valid
+    end
+
+    example "passwordは必須、6文字以上" do
+      user = User.new(name: "山田", email: "test@example.com", password: "")
+      expect(user).to be_invalid
+
+      user = User.new(name: "山田", email: "test@example.com", password: "a" * 5)
+      expect(user).to be_invalid
+
+      user = User.new(name: "山田", email: "test@example.com", password: "password")
+      expect(user).to be_valid
+    end
+  end
 end
