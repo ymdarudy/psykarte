18cc0c6c5c57be33975d059892c865c0e5c117b0
yuji yamada <ymdarudy@gmail.com>
2022-08-24 22:31:19 +0900

    :rotating_light: Categoryモデルスペック記載

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :category do
-    
+    name { "カテゴリ1" }
   end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  example "nameは必須" do
+    category = FactoryBot.build(:category, name: "")
+    expect(category).to be_invalid
+
+    category = FactoryBot.build(:category, name: "カテゴリテスト")
+    expect(category).to be_valid
+  end
 end
