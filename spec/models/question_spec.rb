2bf66aab2048cd1378efd4ab5e520c1d7bda44ef
yuji yamada <ymdarudy@gmail.com>
2022-08-24 22:51:44 +0900

    :recycle: describe記載漏れを修正

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,70 +1,72 @@
 require "rails_helper"
 
 RSpec.describe Question, type: :model do
-  example "titleは必須" do
-    question = FactoryBot.build(:question, title: "")
-    expect(question).to be_invalid
+  describe "validation" do
+    example "titleは必須" do
+      question = FactoryBot.build(:question, title: "")
+      expect(question).to be_invalid
 
-    question = FactoryBot.build(:question, title: "設問1")
-    expect(question).to be_valid
-  end
+      question = FactoryBot.build(:question, title: "設問1")
+      expect(question).to be_valid
+    end
 
-  example "choice_min_pointは必須、かつ整数のみ" do
-    question = FactoryBot.build(:question, choice_min_point: "")
-    expect(question).to be_invalid
+    example "choice_min_pointは必須、かつ整数のみ" do
+      question = FactoryBot.build(:question, choice_min_point: "")
+      expect(question).to be_invalid
 
-    question = FactoryBot.build(:question, choice_min_point: 1.5)
-    expect(question).to be_invalid
+      question = FactoryBot.build(:question, choice_min_point: 1.5)
+      expect(question).to be_invalid
 
-    question = FactoryBot.build(:question, choice_min_point: 2)
-    expect(question).to be_valid
-  end
+      question = FactoryBot.build(:question, choice_min_point: 2)
+      expect(question).to be_valid
+    end
 
-  example "choice_max_pointは必須、かつ整数のみ" do
-    question = FactoryBot.build(:question, choice_max_point: "")
-    expect(question).to be_invalid
+    example "choice_max_pointは必須、かつ整数のみ" do
+      question = FactoryBot.build(:question, choice_max_point: "")
+      expect(question).to be_invalid
 
-    question = FactoryBot.build(:question, choice_max_point: 3.5)
-    expect(question).to be_invalid
+      question = FactoryBot.build(:question, choice_max_point: 3.5)
+      expect(question).to be_invalid
 
-    question = FactoryBot.build(:question, choice_max_point: 4)
-    expect(question).to be_valid
-  end
+      question = FactoryBot.build(:question, choice_max_point: 4)
+      expect(question).to be_valid
+    end
 
-  example "choice_min_wordは必須、かつ30文字以内" do
-    question = FactoryBot.build(:question, choice_min_word: "")
-    expect(question).to be_invalid
+    example "choice_min_wordは必須、かつ30文字以内" do
+      question = FactoryBot.build(:question, choice_min_word: "")
+      expect(question).to be_invalid
 
-    question = FactoryBot.build(:question, choice_min_word: "a" * 31)
-    expect(question).to be_invalid
+      question = FactoryBot.build(:question, choice_min_word: "a" * 31)
+      expect(question).to be_invalid
 
-    question = FactoryBot.build(:question, choice_min_word: "下限文言")
-    expect(question).to be_valid
-  end
+      question = FactoryBot.build(:question, choice_min_word: "下限文言")
+      expect(question).to be_valid
+    end
 
-  example "choice_max_wordは必須、かつ30文字以内" do
-    question = FactoryBot.build(:question, choice_max_word: "")
-    expect(question).to be_invalid
+    example "choice_max_wordは必須、かつ30文字以内" do
+      question = FactoryBot.build(:question, choice_max_word: "")
+      expect(question).to be_invalid
 
-    question = FactoryBot.build(:question, choice_max_word: "a" * 31)
-    expect(question).to be_invalid
+      question = FactoryBot.build(:question, choice_max_word: "a" * 31)
+      expect(question).to be_invalid
 
-    question = FactoryBot.build(:question, choice_max_word: "上限文言")
-    expect(question).to be_valid
-  end
+      question = FactoryBot.build(:question, choice_max_word: "上限文言")
+      expect(question).to be_valid
+    end
 
-  example "point_reversalはtrue,またはfalse" do
-    question = FactoryBot.build(:question, point_reversal: "")
-    expect(question).to be_invalid
+    example "point_reversalはtrue,またはfalse" do
+      question = FactoryBot.build(:question, point_reversal: "")
+      expect(question).to be_invalid
 
-    question = FactoryBot.build(:question, point_reversal: nil)
-    expect(question).to be_invalid
+      question = FactoryBot.build(:question, point_reversal: nil)
+      expect(question).to be_invalid
 
-    question = FactoryBot.build(:question, point_reversal: true)
-    expect(question).to be_valid
+      question = FactoryBot.build(:question, point_reversal: true)
+      expect(question).to be_valid
 
-    question = FactoryBot.build(:question, point_reversal: false)
-    expect(question).to be_valid
+      question = FactoryBot.build(:question, point_reversal: false)
+      expect(question).to be_valid
+    end
   end
 
   describe "モデルのメソッド" do
