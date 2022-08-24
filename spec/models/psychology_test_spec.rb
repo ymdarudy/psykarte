786536870e4821804b7b1cb99d20e78714ed1755
yuji yamada <ymdarudy@gmail.com>
2022-08-24 18:19:42 +0900

    :rotating_light: PsychologyTestモデルスペック記載

--- spec/models/psychology_test_spec.rb
+++ spec/models/psychology_test_spec.rb
@@ -1,5 +1,32 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe PsychologyTest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+    example "titleは必須、かつ255文字以下" do
+      psychology_test = PsychologyTest.new(title: "", description: "説明", referrer: "参照元")
+      expect(psychology_test).to be_invalid
+
+      psychology_test = PsychologyTest.new(title: "a" * 256, description: "説明", referrer: "参照元")
+      expect(psychology_test).to be_invalid
+
+      psychology_test = PsychologyTest.new(title: "タイトル", description: "説明", referrer: "参照元")
+      expect(psychology_test).to be_valid
+    end
+
+    example "descriptionは必須" do
+      psychology_test = PsychologyTest.new(title: "タイトル", description: "", referrer: "参照元")
+      expect(psychology_test).to be_invalid
+
+      psychology_test = PsychologyTest.new(title: "タイトル", description: "説明", referrer: "参照元")
+      expect(psychology_test).to be_valid
+    end
+
+    example "referrerは必須" do
+      psychology_test = PsychologyTest.new(title: "タイトル", description: " 説明", referrer: "")
+      expect(psychology_test).to be_invalid
+
+      psychology_test = PsychologyTest.new(title: "タイトル", description: "説明", referrer: "参照元")
+      expect(psychology_test).to be_valid
+    end
+  end
 end
