d67b0061b7aef2aa15d5c4343c7b3debcf940c47
yuji yamada <ymdarudy@gmail.com>
2022-08-25 16:09:29 +0900

    :bug: 心理テスト回答、結果表示周りのコード修正（answerのshowの挙動がおかしい場合があるため）

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,28 +1,20 @@
 class AnswersController < ApplicationController
   def create
-    questions = Question.where(personality: Personality.where(psychology_test_id: session[:psychology_test_id]))
-    if questions.count != params[:answer]&.keys&.size
+    questions = Question.where(personality: Personality.where(psychology_test_id: params[:answer][:psychology_test_id]))
+    # binding.irb
+    if questions.count != (params[:answer]&.keys&.size - 1)
       redirect_to request.referrer, notice: "未回答の設問がありました。すべて回答してください。"
-      # TODO: 完成後に消去。
-      # @psychology_test = PsychologyTest.find(session[:psychology_test_id])
-      # @answer = Answer.new
-      # flash.now[:alert] = "未回答の設問があります。すべて回答してください。"
-      # render "psychology_tests/show"
     else
       questions.each do |question|
         Answer.create(user: current_user, question: question, point: params[:answer]["#{question.id}"])
       end
-      redirect_to answer_path(session[:psychology_test_id]), notice: "回答を記録しました。"
+      redirect_to answer_path(params[:answer][:psychology_test_id]), notice: "回答を記録しました。"
     end
-    # TODO: 完成後に消去。↓の書き方だとQuestionのSQLが無駄に発行される様子
-    # question_ids = Question.where(personality: PsychologyTest.find(session[:psychology_test_id]).personalities).pluck(:id)
-    # question_ids.each do |q_id|
-    #   Answer.create(user: current_user, question_id: q_id, point: params[:answer]["#{q_id}"])
-    # end
   end
 
   def show
-    session[:psychology_test_id] ||= params[:id]
-    @psychology_test = PsychologyTest.find(session[:psychology_test_id])
+    @psychology_test = PsychologyTest.find(params[:id])
+    return unless current_user.answers.scoped_by_psychology_test(@psychology_test).size == 0
+    redirect_to user_path(current_user), notice: "その心理テストはまだ一度も回答していません。"
   end
 end
