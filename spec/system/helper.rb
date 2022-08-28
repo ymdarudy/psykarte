c6e5a500fa9003a5666ae677f430ca4437c2ba2a
yuji yamada <ymdarudy@gmail.com>
2022-08-28 20:28:04 +0900

    :rotating_light: システムスペック修正（カテゴリ検索テストのためCapybara.rb追加）

--- spec/system/helper.rb
+++ spec/system/helper.rb
@@ -1,8 +1,8 @@
 module RSpecHelper
   def log_in(user)
     visit new_user_session_path
-    fill_in :user_email, with: user.email
-    fill_in :user_password, with: user.password
+    fill_in "user[email]", with: user.email
+    fill_in "user[password]", with: user.password
     click_on "commit"
   end
 end
