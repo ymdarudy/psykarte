d0db2bca96175dbbb1b6eb7bd2497a3aa435c7e9
yuji yamada <ymdarudy@gmail.com>
2022-08-28 18:12:01 +0900

    :bug: jsをturbolinks対応に修正

--- app/javascript/packs/header_padding.js
+++ app/javascript/packs/header_padding.js
@@ -1,4 +1,11 @@
-$(window).on("load resize", function () {
+$(document).on("turbolinks:load", function () {
   var height = 20 + $(".navbar").height();
   $("body").css("padding-top", height);
+  console.log(height);
+});
+
+$(window).resize(function () {
+  var height = 20 + $(".navbar").height();
+  $("body").css("padding-top", height);
+  console.log(height);
 });
