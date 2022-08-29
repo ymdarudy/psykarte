2104c412debcaf24dfb7dacf14a9a51ad32598b2
yuji yamada <ymdarudy@gmail.com>
2022-08-29 23:16:56 +0900

    :bug: 採点方式が平均の場合の小数点表示と、グラフ最高点計算ロジック修正

--- db/seeds/20220829.rb
+++ db/seeds/20220829.rb
@@ -4,7 +4,7 @@ Category.create(id: 6, name: "恋愛")
 ppr = PsychologyTest.create(
   title: "PPR（パートナー応答性知覚）診断",
   description: "パートナーの応答性（反応の良さ）を16問で検査します。",
-  how_to_answer: "あなたの身近なパートナーを思い浮かべつつ、それぞれの項目に、0（まったく当てはまらない）〜5(完全に当てはまる)の範囲で採点してください。\n各項目の前にある「◯◯」には、パートナーの名前を記入しましょう。",
+  how_to_answer: "あなたの身近なパートナーを思い浮かべつつ、それぞれの項目に、0（まったく当てはまらない）〜5(完全に当てはまる)の範囲で採点してください。\n各項目の前にある「◯◯」には、パートナーの名前を入れましょう。",
   referrer: "満足度が高いカップルに共通する最大の要素「PPR」をチェックする16問",
   referrer_url: "https://yuchrszk.blogspot.com/2021/06/ppr16.html",
   category_ids: [6],
@@ -13,14 +13,14 @@ ppr = PsychologyTest.create(
 responsiveness = Personality.create(
   psychology_test: ppr,
   name: "応答性",
-  description: "点数が高いほどちゃんとこちらの話を聞いてくれている（調査平均: 3.5）",
+  description: "点数が高いほど、ちゃんとこちらの話を聞いてくれている（調査平均: 3.5）",
   scoring_system: 2,
 )
 
 insensitivity = Personality.create(
   psychology_test: ppr,
   name: "鈍感性",
-  description: "点数が高いほどこちらの感情に鈍感である（＝点数が低いほど敏感）（調査平均: 1.0）",
+  description: "点数が高いほど、こちらの感情に鈍感である（＝点数が低いほど敏感）（調査平均: 1.0）",
   scoring_system: 2,
 )
 
