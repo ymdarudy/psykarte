87d0db3f7890b7af746d6970b896b7b7630f9eb5
yuji yamada <ymdarudy@gmail.com>
2022-09-05 18:26:39 +0900

    :sparkles: 心理テスト一覧はログインせずに閲覧できるように変更

--- app/controllers/psychology_tests_controller.rb
+++ app/controllers/psychology_tests_controller.rb
@@ -1,14 +1,25 @@
 class PsychologyTestsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+
   def index
     @psychology_tests = PsychologyTest.all
     @test_search = @psychology_tests.ransack(params[:q])
     @psychology_tests = @test_search.result if params[:q].present?
-    @psychology_tests = PsychologyTest.scoped_by_user_unanswered(current_user) if params[:unanswered]
-    @psychology_tests = current_user.favorite_tests if params[:favorite]
+    if params[:unanswered] && current_user.present?
+      @psychology_tests = PsychologyTest.scoped_by_user_unanswered(current_user)
+    elsif params[:unanswered] && current_user.nil?
+      flash.now[:notice] = "ログインすると利用できます。"
+    end
+    if params[:favorite] && current_user.present?
+      @psychology_tests = current_user.favorite_tests
+    elsif params[:favorite] && current_user.nil?
+      flash.now[:notice] = "ログインすると利用できます。"
+    end
   end
 
   def show
     @psychology_test = PsychologyTest.find(params[:id])
     @answer = Answer.new
+    flash.now[:notice] = "ログインすると回答できます。"
   end
 end
